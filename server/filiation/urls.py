30891198c93fdd706659ab14f0f2eab2d13e587d
jjopc <dev.jjopc@gmail.com>
2023-02-26 23:21:45 +0100

    continua el anterior

--- server/filiation/urls.py
+++ server/filiation/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import PatientList
+from .views import PatientList, PatientDetail
 
-urlpatterns = [path("patients/", PatientList.as_view(), name="patients")]
+urlpatterns = [
+    path("patients/", PatientList.as_view(), name="patients"),
+    path("patients/<int:pk>", PatientDetail.as_view(), name="patient_detail"),
+]
--- server/filiation/views.py
+++ server/filiation/views.py
@@ -2,16 +2,26 @@ from django.shortcuts import render
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 
 from filiation.models import Patient
 from filiation.serializers import PatientSerializer
 
 
-class PatientList(APIView):
-    def post(self, request, format=None):
-        serializer = PatientSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class PatientList(APIView):
+#     def post(self, request, format=None):
+#         serializer = PatientSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PatientList(generics.ListCreateAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+
+class PatientDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
