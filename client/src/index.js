93d9c5ff2598bcf515a869e9836c21969b4d1f22
jjopc <dev.jjopc@gmail.com>
2023-02-28 22:51:47 +0100

    Añado muchas dependencias que voy a usar
    - Empiezo a organizar los directorios basados en features

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
-
